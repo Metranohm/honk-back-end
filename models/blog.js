b087e7b7e18a0af500b6699a6801bcab00e18137
Metranohm <andrewwink@gmail.com>
2022-12-22 13:08:10 -0600

    create blogs model

---
+++ models/blog.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    author: { type: Schema.Types.ObjectId, ref: "Profile" },
+  },
+  { timestamps: true }
+);
+
+const BlogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["News", "Sports", "Games", "Politics", "Music", "Movies"],
+    },
+      comments: [commentSchema],
+      author: {
+      type: Schema.Types.ObjectId, ref: "Profile" }
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model('Blog', BlogSchema);
+
+export { Blog };
