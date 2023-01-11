abb88fd8bca8405676d1f8d7825451a0d4713fcb
Metranohm <andrewwink@gmail.com>
2023-01-11 14:01:29 -0600

    create function

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,14 @@ import { Blog } from "../models/blog.js"
 
 const create = async (req, res) => {
   try {
-    
+    req.body.author = req.user.profile
+    const blog = await Blog.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { blogs: blog } },
+      { new: true }
+    )
+    blog
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
